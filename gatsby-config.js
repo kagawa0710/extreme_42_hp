fa46b48c2ec2fd89b7a2b34ebd863ba52527ae87
kagawa0710 <yuuya081@icloud.com>
2021-03-30 11:31:55 +0900

    deploy test

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,20 +7,20 @@ module.exports = {
       resolve: 'gatsby-plugin-jss',
       options: { theme }
     },
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'Extreme_42tokyo',
-        short_name: 'extreme_42',
-        start_url: '/',
-        background_color: '#000000',
-        theme_color: '#000000',
-        display: 'standalone',
-        orientation: 'portrait',
-        // icon: 'src/extreme_42_hp/images/favicon.png'
-      }
-    },
+    // 'gatsby-plugin-react-helmet',
+    // {
+    //   resolve: 'gatsby-plugin-manifest',
+    //   options: {
+    //     name: 'Extreme_42tokyo',
+    //     short_name: 'extreme_42',
+    //     start_url: '/',
+    //     background_color: '#000000',
+    //     theme_color: '#000000',
+    //     display: 'standalone',
+    //     orientation: 'portrait',
+    //     // icon: 'src/extreme_42_hp/images/favicon.png'
+    //   }
+    // },
     {
       resolve: `gatsby-plugin-layout`,
       options: {
