4f3c20abe3472d3d7514a53263a02117bbc122af
kagawa0710 <yuuya081@icloud.com>
2021-04-02 15:34:24 +0900

    deploy test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
-import { Header } from '../Header';
-import { Footer } from '../Footer';
+import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
 import { AppContent } from '../AppContent';
 
 class Component extends React.Component {
