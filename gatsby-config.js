7c6dbc68f51d3f9df5bf6b04424f40f2c6d544ad
kagawa0710 <yuuya081@icloud.com>
2021-04-06 16:19:29 +0900

    update gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,31 +7,25 @@ module.exports = {
       resolve: 'gatsby-plugin-jss',
       options: { theme }
     },
-    // 'gatsby-plugin-react-helmet',
-    // {
-    //   resolve: 'gatsby-plugin-manifest',
-    //   options: {
-    //     name: 'Extreme_42tokyo',
-    //     short_name: 'extreme_42',
-    //     start_url: '/',
-    //     background_color: '#000000',
-    //     theme_color: '#000000',
-    //     display: 'standalone',
-    //     orientation: 'portrait',
-    //     // icon: 'src/extreme_42_hp/images/favicon.png'
-    //   }
-    // },
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Extreme_42tokyo',
+        short_name: 'extreme_42',
+        start_url: '/',
+        background_color: '#000000',
+        theme_color: '#000000',
+        display: 'standalone',
+        orientation: 'portrait',
+        icon: 'src/images/favicon.png'
+      }
+    },
     {
       resolve: `gatsby-plugin-layout`,
       options: {
         component: require.resolve(`./src/layouts/Template`)
       }
     },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'UA-64916263-1'
-      }
-    }
   ]
 };
