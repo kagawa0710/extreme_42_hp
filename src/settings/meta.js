2a5dc0d57b6f820dc4e7241fc00ce666f4002034
kagawa0710 <yuuya081@icloud.com>
2021-04-13 13:55:57 +0900

    deploy test

--- src/settings/meta.js
+++ src/settings/meta.js
@@ -2,7 +2,7 @@ module.exports = {
   title: '42_extreme',
   description: '',
   keywords: '',
-  url: 'https://kagawa0710.github.io/extreme_42_hp/',
+  url: 'https://kagawa0710.github.io/',
   twitter: '@42_extreme',
   seoImage: 'https://42_extreme.com/extreme_42_hp/images/background.jpg',
   color: '#000000'
