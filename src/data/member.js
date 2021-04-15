c5e673bbfd267a2fc20eb6eac549c069ca45c067
kagawa0710 <yuuya081@icloud.com>
2021-04-15 11:03:54 +0900

    update member

--- src/data/member.js
+++ src/data/member.js
@@ -3,14 +3,12 @@
 
 module.exports = [
     {
-        name: 'kagawa yuya',
         intra_name: 'ykagawa',
         message: `Leader`,
         image: 'https://pbs.twimg.com/profile_images/1283055935087349766/NakVKBWW.jpg',
         link: 'https://twitter.com/yuuya08110'
     },
     {
-        name: 'kurosu sho',
         intra_name: 'skurosu tech',
         message: `I got a job`,
         image: 'https://pbs.twimg.com/profile_images/1320210198036762624/bJuKGt3H.jpg',
@@ -27,6 +25,36 @@ module.exports = [
         message: `In hyougo`,
         image: 'https://pbs.twimg.com/profile_images/1348940660468969476/jBMjPewe_400x400.jpg',
         link: 'https://twitter.com/yuuuki81?s=20'
+    },
+    {
+        intra_name: 'yhamakay',
+        message: `I have a very cute girlfriend.`,
+        image: 'https://pbs.twimg.com/profile_images/1362182832454426624/bYHWCBVu_400x400.jpg',
+        link: 'https://twitter.com/yhakamay'
+    },
+    {
+        intra_name: 'otaishi',
+        message: `I lobe camp`,
+        image: 'https://pbs.twimg.com/profile_images/1326982171043622912/rqwjb3G4_400x400.jpg',
+        link: 'https://twitter.com/otaishi2000'
+    },
+    {
+        intra_name: 'yhasegawa',
+        message: `Spoofing age`,
+        image: 'https://pbs.twimg.com/profile_images/1289190833363668992/6Jj7a-hs_400x400.jpg',
+        link: 'https://twitter.com/8Piscine1'
+    },
+    {
+        intra_name: 'unlimish',
+        message: `I'm a guru`,
+        image: 'https://pbs.twimg.com/profile_images/1314112304682680321/8PjCZ_XD_400x400.jpg',
+        link: 'https://twitter.com/_unlimish'
+    },
+    {
+        intra_name: 'sleepyfox',
+        message: `I always want to sleep`,
+        image: 'https://pbs.twimg.com/profile_images/1369297003373531143/8OKHVYz-_400x400.jpg',
+        link: 'https://twitter.com/sleepyfox_42'
     },
 
   // templete
